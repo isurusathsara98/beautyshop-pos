560be79be14ce9f59977c10c8f2d4b7705e779ad
Gihan Chathuranga <gihan.c@creativesoftware.com>
2024-03-04 15:38:45 +0530

    add analytics page

---
+++ src/Pages/Analytics.razor.cs
@@ -0,0 +1,45 @@
+﻿using WomenBeautyBoutique.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace WomenBeautyBoutique.Pages
+{
+    public partial class Analytics
+    {
+        private List<TransactionItem> transactionItems = new List<TransactionItem>();
+        private DateTime SelectedDateTimeFrom { get; set; }
+        private DateTime SelectedDateTimeTo { get; set; }
+
+        private int selectedButtonIndex = -1;
+        protected override async Task OnInitializedAsync()
+        {
+            transactionItems = await JS.InvokeAsync<List<TransactionItem>>("getTransaction");
+            foreach (var transactionItem in transactionItems)
+            {
+
+            }
+        }
+        private string HighlightButton(int buttonIndex)
+        {
+            return selectedButtonIndex == buttonIndex ? "background-color: #218838 !important;" : "";
+        }
+        private async void SetLastDay()
+        {
+            ToastService.ShowInfo("Set Date Time for Last Day!");
+            selectedButtonIndex = 0;
+            StateHasChanged();
+        }
+        private async void SetLastWeek()
+        {
+            ToastService.ShowInfo("Set Date Time for Last Week!");
+            selectedButtonIndex = 1;
+            StateHasChanged();
+        }
+        private async void SetLastMonth()
+        {
+            ToastService.ShowInfo("Set Date Time for Last Month!");
+            selectedButtonIndex = 2;
+            StateHasChanged();
+        }
+    }
+}
